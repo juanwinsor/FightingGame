9bdd2a552ec91bdda6f4c8db626377b716adfd89
Ernesto <echapa.silva1990@gmail.com>
2015-11-15 17:11:50 -0500

    Made Idle Animation Make walk

---
+++ Assets/Scripts/Characters/TempDude/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController characterController;
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            characterController.Move(MoveDirection.Right);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            characterController.Move(MoveDirection.Left);
+        }
+
+        else if (Input.GetKey(KeyCode.D) == false && Input.GetKey(KeyCode.A) == false)
+        {
+            characterController.Move(MoveDirection.Idle);
+        }
+
+    }
+}
